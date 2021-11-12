b628461104504ddf2654252d8a01b8fffffde421
acholonu <ucacholonu@hotmail.com>
2021-11-12 17:57:26 -0600

    Setup Docker file

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -
 
 # install pyenv & virtualenvs
+
+# How to run the docker files
+docker build -f Dagster.Dockerfile .
+docker build -f DB.Dockerfile .
