eaf835704bdb220ec9f84949b940204f38020326
acholonu <ucacholonu@hotmail.com>
2021-11-26 16:02:37 -0600

    Working with a repository

--- dagster/dagster_capstone/repository.py
+++ dagster/dagster_capstone/repository.py
@@ -1,9 +1,15 @@
-from dagster import repository
+"""repository.py
+    Not sure if I fully understand this file.  I think it just groups jobs, schedules, steps into a repository,
+    so you can call it inside of dagit.  The documentation is not that clear. 
 
+    Reference: https://docs.dagster.io/concepts/repositories-workspaces/repositories#repositories
+
+"""
+from dagster import repository
+from dagster_capstone.hello_world import diamond
 from dagster_capstone.graphs.say_hello import say_hello_job
 from dagster_capstone.schedules.my_hourly_schedule import my_hourly_schedule
 from dagster_capstone.sensors.my_sensor import my_sensor
-
 
 @repository
 def dagster_capstone():
@@ -13,7 +19,7 @@ def dagster_capstone():
     For hints on building your Dagster repository, see our documentation overview on Repositories:
     https://docs.dagster.io/overview/repositories-workspaces/repositories
     """
-    jobs = [say_hello_job]
+    jobs = [say_hello_job, diamond]
     schedules = [my_hourly_schedule]
     sensors = [my_sensor]
 
