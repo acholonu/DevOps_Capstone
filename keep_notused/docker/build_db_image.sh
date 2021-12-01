40299f1a2b68a1a2abb172c78501a599679c9eeb
acholonu <ucacholonu@hotmail.com>
2021-12-01 06:56:17 -0600

    Developing the Dagster Database

--- keep_notused/docker/build_db_image.sh
+++ keep_notused/docker/build_db_image.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
+
 # Create Docker Image for Postgres Database Service
 # ------------------------------------------------
 # Args:
 # -----
 # $1 = true if you want Create Volumes
-# $2 = Use a cache path
 #
 # How to run
 # ----------
@@ -41,5 +42,22 @@ fi
 # Note, I already pulled the docker image to my local image repository
 # I used the command: `docker pull postgres`.  It is the official postgres
 # container. url: https://hub.docker.com/_/postgres
+#
+# Side note:
+# the \ (i.e. backslash) at the end says ignore the next character. In this
+# case the next character was \n (i.e. new line).  However, I was running into an error earlier
+# where the following command was not working.  The issue was that I had a space
+# and then a \n. So the command ignore the space and not the \n.  So the overall
+# command would fail.  I had to remove the space. 
+docker run --rm -d --name dagster_db_ctnr -p 5432:5432 \
+    -v postgres_data:/var/lib/postgresql/data \
+    -v postgres_config:/etc/postgresql \
+    --network postgres_network \
+    -e POSTGRES_PASSWORD=$POSTGRES_SUPER_USER_PASSWORD \
+    -e POSTGRES_USER=$POSTGRES_SUPER_USER \
+    -e POSTGRES_DB=$POSTGRES_DATABASE \
+    postgres
+
+# Note -e says this is an environment variable
 
 # Here is where I left off: https://docs.docker.com/language/python/develop/#:~:text=Now%20we%20can%20run,%24
