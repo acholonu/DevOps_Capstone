612a15329c5d1867906ad25c92bdf8bba02b7776
acholonu <ucacholonu@hotmail.com>
2021-12-03 22:59:39 -0600

    Added docker-compose

--- cloudformation/create_stack.sh
+++ cloudformation/create_stack.sh
@@ -1,1 +1,11 @@
-aws cloudformation create-stack --profile=udacity_devops --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+# How to use:
+# ------------
+# ./create_stack.sh default dagster-network app_network.yaml app_network_parameters.json
+#
+aws cloudformation create-stack \
+--profile=$1 \
+--stack-name $2 \
+--template-body file://$3 \
+--parameters file://$3 \
+--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+--region=us-east-2
